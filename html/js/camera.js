e3d4a8f58593b60784ce8bdaa2bed9c7c74f038a
Paul Cooley <pcooley@gmail.com>
2013-12-27 06:30:23 -0800

    Implement outline the FI8918W implementation

--- html/js/camera.js
+++ html/js/camera.js
@@ -1,45 +1,83 @@
 function Camera (
-    Config
+    Config, CamImpl
 ) {
 	this.config = Config;
+	this.camera = CamImpl;
 
 	this.left = function() {
+		this.camera.left();
 		if( console && console.log ) {
 	            console.log("left:" + config['server']);
 	            }
 	};
 
 	this.right = function() {
+		this.camera.right();	
 		if( console && console.log ) {
 	            console.log("right" + config['server']);
 	            }
 	};
 	
 	this.up = function() {
+		this.camera.up();	
 		if( console && console.log ) {
 	            console.log("up" + config['server']);
 	            }
 	};
 	
 	this.down = function() {
+		this.camera.down();
 		if( console && console.log ) {
 	            console.log("down" + config['server']);
 	            }
 	};
 	
+	
+	this.up_left = function() {
+		this.camera.up_left();
+		if( console && console.log ) {
+	            console.log("up_left:" + config['server']);
+	            }
+	};
+
+
+	this.up_right = function() {
+		this.camera.up_right();
+		if( console && console.log ) {
+	            console.log("up_right" + config['server']);
+	            }
+	};
+	
+	this.down_left = function() {
+		this.camera.down_left();
+		if( console && console.log ) {
+	            console.log("down_left" + config['server']);
+	            }
+	};
+	
+	this.down_right = function() {
+		this.camera.down_right();
+		if( console && console.log ) {
+	            console.log("down_right" + config['server']);
+	            }
+	};
+
+	
 	this.stop =  function() {
+		this.camera.stop();
 		if( console && console.log ) {
 	            console.log("stop" + config['server']);
 	            }
 	};
-	
 	this.ir_on =  function() {
+		this.camera.ir_on();
 		if( console && console.log ) {
 	            console.log("ir_on" + config['server']);
 	            }
 	};
 	
 	this.ir_off =  function() {
+		this.camera.ir_off();
 		if( console && console.log ) {
 	            console.log("ir_off" + config['server']);
 	            }
---
+++ html/js/fi8918w.js
@@ -0,0 +1,38 @@
+function FI8918W () {
+
+	this.left = function() {
+	};
+
+	this.right = function() {
+	};
+	
+	this.up = function() {
+	};
+	
+	this.down = function() {
+	};
+	
+	
+	this.up_left = function() {
+	};
+
+
+	this.up_right = function() {
+	};
+	
+	this.down_left = function() {
+	};
+	
+	this.down_right = function() {
+	};
+
+	
+	this.stop =  function() {
+	};
+	this.ir_on =  function() {
+	};
+	
+	this.ir_off =  function() {
+	};
+
+}
