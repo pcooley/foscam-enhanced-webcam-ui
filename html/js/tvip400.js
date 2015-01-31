4c0c6aeba658010605f9ff2e7252660c0c9ec770
Paul Cooley <pcooley@gmail.com>
2015-01-30 21:10:34 -0800

    Update with the static JPG and preset URL

--- html/js/tvip400.js
+++ html/js/tvip400.js
@@ -2,17 +2,26 @@ function TVIP400 () {
 
 	this.APIURLCONST = "PANTILTCONTROL.CGI?";
 	this.STREAMURLCONST = "video.cgi?";
-	this.SNAPSHOPURLCONST = "snapshot.cgi?";
+	this.SNAPSHOPURLCONST = "IMAGE.JPG?";
 	this.STOPDELAY = 1000;
 	this.presetMap = {
-		"1" : "31",
-		"2" : "33",
-		"3" : "35",
-		"4" : "37",
-		"5" : "39",
-		"6" : "41",
-		"7" : "43",
-		"8" : "45"
+		"1" : "0",
+		"2" : "1",
+		"3" : "2",
+		"4" : "3",
+		"5" : "4",
+		"6" : "5",
+		"7" : "6",
+		"8" : "7",
+		"9" : "8",
+		"10" : "9",
+		"11" : "10",
+		"12" : "11",
+		"13" : "12",
+		"14" : "13",
+		"15" : "14",
+		"16" : "15",
+		"17" : "16"
 		}
 	
 	this.getVideoStreamEndPoint = function() {
@@ -80,22 +89,28 @@ function TVIP400 () {
 	}
 	
 	this.ir_on =  function(baseURL, queryString) {
-		this.makeAjaxRequest(baseURL, queryString, "95");
 	};
 	
 	this.ir_off =  function(baseURL, queryString) {
-		this.makeAjaxRequest(baseURL, queryString, "94");
 	};
 	
 	this.goto_preset =  function(baseURL, queryString, presetNumber) {
 		if (presetNumber in this.presetMap) {
-			this.makeAjaxRequest(baseURL, queryString, this.presetMap[presetNumber]);
+			this.makePreSETAjaxRequest(baseURL, queryString, this.presetMap[presetNumber]);
 			}
 		
 	};
-
 	this.makeAjaxRequest = function(baseURL, queryString, commandNum) {
 		data = {PanSingleMoveDegree: 1, TiltSingleMoveDegree: 1, PanTiltSingleMove: commandNum}
+		$.ajax({
+			type: "POST",
+			url: baseURL + this.APIURLCONST + commandNum + "&" + queryString,
+			data: data
+		});
+	};
+	
+	this.makePreSETAjaxRequest = function(baseURL, queryString, commandNum) {
+		data = {PanTiltPresetPositionMove: commandNum}
 		$.ajax({
 			type: "POST",
 			url: baseURL + this.APIURLCONST + commandNum + "&" + queryString,
